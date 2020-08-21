c6ee8f53641a7d773afd228e4be431125eaf332d
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-21 08:36:44 -0600

    Add project files.

---
+++ Variables.vb
@@ -0,0 +1,30 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Variables
+
+    Sub Main()
+        Dim myFirstNumber As Integer
+        Dim mySecondNumber As Integer
+        Dim aLittleString As String
+        Dim someSortOfTest As Boolean
+
+        myFirstNumber = 42
+        mySecondNumber = 0
+
+        Console.WriteLine(myFirstNumber)
+        Console.ReadLine()
+
+        aLittleString = "Some text"
+        Console.WriteLine(aLittleString)
+        Console.ReadLine()
+
+        someSortOfTest = True
+        Console.WriteLine(someSortOfTest)
+        Console.ReadLine()
+
+        'x = 7
+
+    End Sub
+
+End Module
